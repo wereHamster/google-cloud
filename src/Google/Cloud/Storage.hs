7b52760e2565e849cab80543fba0c4f1c8e29474
dpetrov <dpetrov@mabius.ru>
2016-05-20 12:53:51 +0300

    Add downloadMedia function.

--- src/Google/Cloud/Storage.hs
+++ src/Google/Cloud/Storage.hs
@@ -5,13 +5,16 @@ module Google.Cloud.Storage where
 
 import Control.Monad
 
+import Network.HTTP.Types (Header)
 import Data.ByteString (ByteString)
 import Data.Monoid
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC8
 
 import Google.Cloud.Internal.Types
 import Google.Cloud.Internal.HTTP
 import Google.Cloud.Internal.Token
-
 
 
 newtype Bucket = Bucket { unBucket :: String }
@@ -21,9 +24,27 @@ newtype Name = Name { unName :: String }
 
 -- | Upload a 'ByteString' to a 'Bucket'. This is the simplest function
 -- to upload something into a 'Bucket'.
-uploadMedia :: Bucket -> Name -> ByteString -> ByteString -> Cloud ()
-uploadMedia bucket name body contentType = do
+uploadMedia :: Bucket -> Name -> ByteString -> [Header] -> Cloud ()
+uploadMedia bucket name body header = do
     authH <- authorizationHeader
-    void $ post url [("Content-Type", contentType), authH] body
+    void $ post url (contentLength : authH : header) body
   where
-    url = "https://www.googleapis.com/upload/storage/v1/b/" <> unBucket bucket <> "/o?uploadType=media&name=" <> unName name
+    url =
+        "https://www.googleapis.com/upload/storage/v1/b/" <> unBucket bucket <>
+        "/o?uploadType=media&name=" <>
+        unName name
+    contentLength = ("Content-Length", BSC8.pack (show (BS.length body)))
+
+
+-- | Donwload a 'ByteString' from a 'Bucket'.
+-- See https://cloud.google.com/storage/docs/json_api/v1/how-tos/performance for set `Header`
+downloadMedia :: Bucket -> Name -> [Header] -> Cloud ByteString
+downloadMedia bucket name header = do
+    authH <- authorizationHeader
+    get url (authH : header)
+  where
+    url = storageUrl <> "b/" <> unBucket bucket <> "/o/" <> unName name
+
+
+storageUrl :: String
+storageUrl = "https://www.googleapis.com/storage/v1/"
